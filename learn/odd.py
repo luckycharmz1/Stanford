51ef4750df00915cad55c4d1e0d1e26372da68b7
Charmaine Washington <cwashington2011@outlook.com>
2024-07-12 12:44:05 -0700

    added a file

--- learn/odd.py
+++ learn/odd.py
@@ -1,5 +1,3 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 from datetime import datetime
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
